f76519ca40fd48fd7992ca2ef405f88d0dae2d4e
guy <guyaluk@gmail.com>
2016-12-13 23:34:43 +0200

    Test await function fix

--- src/test/java/TestVersionMonitor.java
+++ src/test/java/TestVersionMonitor.java
@@ -4,14 +4,19 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class TestVersionMonitor {
-    VersionMonitor vs;
-    Thread thread1;
-    Thread thread2;
-    Thread thread3;
+    private VersionMonitor vs;
+    private Thread thread0;
+    private Thread thread1;
+    private Thread thread2;
+    private Thread thread3;
 
     @Before
     public void setUp() {
         vs = new VersionMonitor();
+        thread0 = null;
+        thread1 = null;
+        thread2 = null;
+        thread3 = null;
     }
 
     @After
@@ -48,35 +53,34 @@ public class TestVersionMonitor {
         } catch (InterruptedException e) {
 
         }
-        assertEquals(300,vs.getVersion());
+        assertEquals(300, vs.getVersion());
     }
 
     @Test
     public void await() {
-        thread1 = new Thread(()-> {
+        thread0 = new Thread(() -> {
             try {
-                thread1.sleep(10000);
+                thread1.sleep(3000);
+            } catch (InterruptedException e) {
             }
-            catch(InterruptedException e){}
-            inc();
+            vs.inc();
         });
-        thread2 = new Thread(()-> {
+        thread1 = new Thread(() -> {
             try {
                 vs.await(0);
-            }
-            catch(InterruptedException e){
-                vs.inc();
-            }
-            thread1.start();
-            thread2.start();
-            try {
-                thread1.join();
-                thread2.join();
             } catch (InterruptedException e) {
-
             }
-            assertEquals(2,vs.getVersion());
+            vs.inc();
         });
+        thread0.start();
+        thread1.start();
+        try {
+            thread0.join();
+            thread1.join();
+        } catch (InterruptedException e) {
+
+        }
+        assertEquals(2, vs.getVersion());
     }
 
 }
